1d7329522f96ca1639c750fc8e1ccd6c596afe94
lamy-m <131240847+lamy-m@users.noreply.github.com>
2023-06-06 16:52:13 +0200

    changed description

--- R/Celegans_norm.R
+++ R/Celegans_norm.R
@@ -2,6 +2,8 @@
 #'
 #' @param type mean value of this vehicle will be set to 1. All the other values will be expressed as a fraction thereof.
 #'
+#' @importFrom utils View
+#' @importFrom dplyr select
 #' @return colomn with normalized data
 #' @export
 #'
@@ -11,9 +13,16 @@
 #' Celegans_norm("controlPositive")
 #' Celegans_norm("controlVehicleA")
 Celegans_norm <- function(type){
+  vialNr <- NULL
+  expType <- NULL
+  expName <- NULL
+  RawData <- NULL
+  compName <- NULL
+  compConcentration <- NULL
+  compVehicle <- NULL
+  data_norm <- NULL
   type_mean <- mean(Celegans$RawData[Celegans$expType == type])
-
   Celegans$data_norm <- Celegans$RawData/type_mean
-
+  Celegans <- Celegans %>% select( vialNr, expType, expName, RawData, compName, compConcentration, compVehicle, data_norm )
   return(Celegans)
   }
